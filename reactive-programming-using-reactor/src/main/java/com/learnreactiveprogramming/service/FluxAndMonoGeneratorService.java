3c9ecae711973077da033c7b82c238f8b09a7718
Robson Paixão <robson.emanuelpx@gmail.com>
2024-11-08 07:13:25 -0300

    creating my fist mono/flux method

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,27 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> fluxNames() {
+        return Flux.fromIterable(List.of("rob", "tay", "vini"));
+    }
+
+    public Mono<String> fluxName() {
+        return Mono.just("rob");
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.fluxNames()
+                .subscribe(name -> System.out.println("Name is: " + name));
+
+
+        fluxAndMonoGeneratorService.fluxName()
+                .subscribe(name -> System.out.println("Mono name is: " + name));
+    }
 }
