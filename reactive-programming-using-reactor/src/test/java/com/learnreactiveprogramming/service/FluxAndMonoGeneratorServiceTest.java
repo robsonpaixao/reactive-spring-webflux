545df96ccb296ad1575f0baaac20004cb876b746
Robson Paixão <robson.emanuelpx@gmail.com>
2024-11-08 07:30:15 -0300

    implement tests

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,29 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+        var namesFlux = fluxAndMonoGeneratorService.fluxNames();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("rob", "tay", "vini")
+                .verifyComplete();
+
+        StepVerifier.create(namesFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("rob")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
 }
