189d97e78e519df2784fd9375bb1cef5b1bea006
Robson Paixão <robson.emanuelpx@gmail.com>
2024-11-19 07:34:36 -0300

    implements update movie reviews endpoint

--- movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
+++ movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
@@ -17,7 +17,8 @@ public class ReviewRouter {
         return route()
                 .nest(path("/v1/reviews"), builder -> {
                     builder.POST("", request -> reviewHandler.addReview(request))
-                            .GET("", request -> reviewHandler.getReviews(request));
+                            .GET("", request -> reviewHandler.getReviews(request))
+                            .PUT("/{id}", request -> reviewHandler.updateReview(request));
                 })
                 .GET("/v1/helloworld", (request -> ServerResponse.ok().bodyValue("Hello World")))
 //                .POST("/v1/reviews", request -> reviewHandler.addReview(request))
